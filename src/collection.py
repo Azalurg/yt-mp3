83c56318c6a13c8f2594ff40f5b1c0da9bf3622e
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-05-31 13:15:40 +0200

    clate class for audio that will be cut

--- src/collection.py
+++ src/collection.py
@@ -1,24 +1,76 @@
+import os
+from typing import List
+
+from pydub import AudioSegment
 from pytube import YouTube
 
-
-def find_it(soure: dict, key: str):
-    if key in soure:
-        return soure[key]
-    for k, v in soure.items():
-        if isinstance(v, dict):
-            item = find_it(v, key)
-            if item:
-                return item
-    return None
+from src.audio import AgAudio
 
 
-url = "https://www.youtube.com/watch?v=b1Fo_M_tj6w"
-yt = YouTube(url)
-data = yt.initial_data
-# /engagementPanels/1/engagementPanelSectionListRenderer/content/macroMarkersListRenderer/contents/0/macroMarkersListItemRenderer/thumbnail/thumbnails
-chapters = data["engagementPanels"][1]["engagementPanelSectionListRenderer"]["content"][
-    "macroMarkersListRenderer"
-]["contents"]
-for chapter in chapters:
-    c = chapter["macroMarkersListItemRenderer"]
-    print(c["title"]["simpleText"], find_it(c, "startTimeSeconds") or 0)
+class AgChapter:
+    def __init__(self, title: str, start: int, end):
+        self.title = title
+        self.start = start
+        self.end = end
+
+    def __str__(self):
+        return f"{self.title} - {self.start} - {self.end}"
+
+
+class AgCollection:
+    def __init__(self, url: str):
+        self.url = url
+        self.yt = YouTube(url)
+        self.data = self.yt.initial_data
+        self.chapters: List[AgChapter] = []
+        self.audio_path = ""
+
+    def find_it(self, source: dict, key: str):
+        if key in source:
+            return source[key]
+        for k, v in source.items():
+            if isinstance(v, dict):
+                item = self.find_it(v, key)
+                if item:
+                    return item
+        return None
+
+    def get_chapters(self):
+        raw_chapters = self.data["engagementPanels"][1][
+            "engagementPanelSectionListRenderer"
+        ]["content"]["macroMarkersListRenderer"]["contents"]
+        for c in raw_chapters:
+            title = c["macroMarkersListItemRenderer"]["title"]["simpleText"]
+            start = self.find_it(c, "startTimeSeconds") or 0
+            self.chapters.append(AgChapter(title, start, 0))
+        for i, c in enumerate(self.chapters):
+            if i + 1 < len(self.chapters):
+                c.end = self.chapters[i + 1].start
+            else:
+                c.end = self.yt.length
+
+    def download_audio(self):
+        self.audio_path = AgAudio(self.url, "/tmp").perform()
+
+    def cut_audio(self):
+        for c in self.chapters:
+            audio = AudioSegment.from_file(self.audio_path, format="mp3")
+            audio = audio[c.start * 1000 : c.end * 1000]
+            audio.export(f"/tmp/music{c.title}.mp3", format="mp3")
+        os.remove(self.audio_path)
+
+    def perform(self):
+        self.get_chapters()
+        self.download_audio()
+        self.cut_audio()
+        return 0
+
+
+# TODO: fix static paths
+# add naming to the files
+
+
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=b1Fo_M_tj6w"
+    ac = AgCollection(url)
+    print(ac.perform())
